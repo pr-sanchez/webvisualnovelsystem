0c6266290e7a7b301295a126884ce960b162c773
Pedro Sánchez <pedrozo_96@hotmail.com>
2018-06-11 09:55:01 -0400

    Add pages

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
     has_many :comments, dependent: :destroy
     validates :title, presence: true, length: { minimum: 5 }
     validates :body, presence: true
+    validates :title, :uniqueness => { :message => "El titulo de esto post ya ha sido tomado" }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+
   resources :posts do
     resources :comments
   end
 
-  root "posts#index"
+  root "pages#home"
+
+  get '/about', to: 'pages#about'
+  get "news" => 'posts#index'
+  get '/novel', to: 'pages#novel'
+
 end
